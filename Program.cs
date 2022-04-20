ac8c571a889f3032d1e58452b4f0145a7815179f
Michael Mathu <mathumichael17@gmail.com>
2022-04-20 10:00:16 +0300

    added base class

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Inheritance
+{
+    //Base Class
+    class Polygon
+    {
+        public int NumberOfSides { get; set; }
+
+        public Polygon()
+        {
+            NumberOfSides = 0;
+        }
+        public Polygon(int numberOfSides)
+        {
+            NumberOfSides = numberOfSides;
+        }
+    }
+}
